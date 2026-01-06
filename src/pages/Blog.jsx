05896dc8ca0d3e58a129f2a15ed12839d012b2fd
Timothy Chu <pepsivstim@gmail.com>
2026-01-06 13:13:05 -0500

    Add blog post carousel

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -56,7 +56,7 @@ function Blog() {
     }, []);
 
     return (
-        <div className="flex-grow bg-paper-base text-ink-black pt-28 pb-8">
+        <div className="flex-grow bg-paper-base text-ink-black pt-20 md:pt-[88px] lg:pt-28 pb-8">
             <div className="w-full max-w-4xl mx-auto px-6 md:px-16 lg:px-8">
                 <div className="space-y-12">
                     {posts.map((post) => (
